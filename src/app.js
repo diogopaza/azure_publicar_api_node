03a9084c30fd08de22800d836fbefa42ff21cbdd
Diogo Paza <diogopazacvel@gmail.com>
2018-05-07 09:36:30 -0300

    inicio

---
+++ src/app.js
@@ -0,0 +1,12 @@
+
+
+
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+const index = require('./routes/index');
+app.use('/', index);
+
+
+module.exports = app;
